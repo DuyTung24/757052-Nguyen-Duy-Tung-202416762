e784dc5f66f197cfeba24a04f5409b01eab2bbe3
DuyTung24 <135224320+DuyTung24@users.noreply.github.com>
2025-10-30 16:44:45 +0700

    Update ConcatenationInLoops.java
    i commited the wrong file, so i have to commit again

--- Lab03/OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
+++ Lab03/OtherProjects/src/hust/soict/dsai/garbage/ConcatenationInLoops.java
@@ -1,5 +1,36 @@
 package hust.soict.dsai.garbage;
 
 public class ConcatenationInLoops {
+    public static void main(String[] args) {
+        int n = 10000;
 
+
+        long start = System.currentTimeMillis();
+        String s = "";
+        for (int i = 0; i < n; i++) {
+            s += "a";
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Time with + operator: " + (end - start) + " ms");
+
+
+        start = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append("a");
+        }
+        String result1 = sb.toString();
+        end = System.currentTimeMillis();
+        System.out.println("Time with StringBuilder: " + (end - start) + " ms");
+
+
+        start = System.currentTimeMillis();
+        StringBuffer sbf = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            sbf.append("a");
+        }
+        String result2 = sbf.toString();
+        end = System.currentTimeMillis();
+        System.out.println("Time with StringBuffer: " + (end - start) + " ms");
+    }
 }
